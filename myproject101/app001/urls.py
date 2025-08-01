6e3e9d3f45212bd3efff8cc50642598b3e87ae7b
Charles Mbuya <charlesmbuya369@gmail.com>
2025-08-01 12:51:13 +0300

    Update URL pattern for login and configure REST framework authentication settings

--- myproject101/app001/urls.py
+++ myproject101/app001/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('createtask/', views.TheTaskList.as_view()),
     path('updatetask/<int:pk>/', views.TheTask.as_view()),
     path('createuser/', views.UserList.as_view()),
-    path('login/', views.LoginUser.as_view()),
+    path('loginuser/', views.LoginUser.as_view()),
 ]
